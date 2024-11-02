e9eeccb9b3a8d81f2d63ef1bce9fe79d91df6155
JuniorQueiroga <antonio.augusto.7@gmail.com>
2024-11-02 11:55:27 -0300

    Update ExemplosForEach.java

--- src/ExemplosForEach.java
+++ src/ExemplosForEach.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class ExemplosForEach {
+    public static void main(String[] args) {
+        System.out.println("***Toda a lista ****");
+
+        List<Pessoa> lista = new Pessoa().populaPessoas();
+
+        lista.stream().forEach(pessoa -> System.out.println(pessoa.getNome()));
+
+        System.out.println("***Apenas sexo feminino ****");
+
+        lista.stream()
+                .filter(pessoa -> pessoa.getSexo().equals("F"))
+                .map(Pessoa::getNome)
+                .forEach(p -> System.out.println(p));
+    }
 }
