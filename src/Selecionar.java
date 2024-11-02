546ab8948e63cbe0c362e27e5a460786e1c790bf
JuniorQueiroga <antonio.augusto.7@gmail.com>
2024-11-02 11:56:11 -0300

    Update Selecionar.java

--- src/Selecionar.java
+++ src/Selecionar.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class Selecionar {
+    public static void main(String[] args) {
+        List<Pessoa> lista = new Pessoa().populaPessoas();
+        Stream<Pessoa> stream = lista.stream().filter(pessoa -> pessoa.getSexo().equals("F"));
+    }
 }
