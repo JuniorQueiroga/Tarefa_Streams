3ed75f87e8b8033c195778bd1e5fe7726abdb1a3
JuniorQueiroga <antonio.augusto.7@gmail.com>
2024-11-02 11:55:09 -0300

    Update ExemplosCollect.java

--- src/ExemplosCollect.java
+++ src/ExemplosCollect.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ExemplosCollect {
+
+    public static void main(String[] args) {
+        List<Pessoa> lista = new Pessoa().populaPessoas();
+
+        List<Pessoa> listaB = lista.stream()
+                .filter(pessoa -> pessoa.getSexo().equals("F"))
+                .collect(Collectors.toList());
+        listaB.forEach(System.out::println);
+
+        System.out.println("***");
+    }
 }
